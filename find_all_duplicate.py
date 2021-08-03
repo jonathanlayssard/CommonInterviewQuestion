22a15411ee89f6627d70e8465c9e625873119e5e
Jonathan Layssard <jonathan.layssard@q2ebanking.com>
2021-08-03 11:03:02 -0500

    removing commented lines that are not neccessary

--- find_all_duplicate.py
+++ find_all_duplicate.py
@@ -1,64 +1,64 @@
 # Python3 implementation of the
 # above approach
-
+#
 # Function to find the Duplicates,
 # if duplicate occurs 2 times or
 # more than 2 times in array so,
 # it will print duplicate value
 # only once at output
-# def findDuplicates(arr, Len):
-#     # Initialize ifPresent as false
-#     ifPresent = False
-#     # ArrayList to store the output
-#     a1 = []
-#     for i in range(Len - 1):
-#         for j in range(i + 1, Len):
-#             # Checking if element is
-#             # present in the ArrayList
-#             # or not if present then break
-#             if (arr[i] == arr[j]):
-#                 if arr[i] in a1:
-#                     break
-#                 # If element is not present in the
-#                 # ArrayList then add it to ArrayList
-#                 # and make ifPresent at true
-#                 else:
-#                     a1.append(arr[i])
-#                     ifPresent = True
-#     # If duplicates is present
-#     # then print ArrayList
-#     if (ifPresent):
-#         print(a1, end = " ")
-#     else:
-#         print("No duplicates present in arrays")
-# # Driver Code
-# arr = [ 12, 11, 40, 12, 5, 6, 5, 12, 11 ]
-# n = len(arr)
-#
-# findDuplicates(arr, n)
+def findDuplicates(arr, Len):
+    # Initialize ifPresent as false
+    ifPresent = False
+    # ArrayList to store the output
+    a1 = []
+    for i in range(Len - 1):
+        for j in range(i + 1, Len):
+            # Checking if element is
+            # present in the ArrayList
+            # or not if present then break
+            if (arr[i] == arr[j]):
+                if arr[i] in a1:
+                    break
+                # If element is not present in the
+                # ArrayList then add it to ArrayList
+                # and make ifPresent at true
+                else:
+                    a1.append(arr[i])
+                    ifPresent = True
+    # If duplicates is present
+    # then print ArrayList
+    if (ifPresent):
+        print(a1, end = " ")
+    else:
+        print("No duplicates present in arrays")
+# Driver Code
+arr = [ 12, 11, 40, 12, 5, 6, 5, 12, 11 ]
+n = len(arr)
+
+findDuplicates(arr, n)
 
 # Python3 program to find duplicates
 # using dictionary approach.
-# def printDuplicates(arr):
-#     dict = {}
-#
-#     for ele in arr:
-#         try:
-#             dict[ele] += 1
-#         except:
-#             dict[ele] = 1
-#
-#     for item in dict:
-#
-#         # if frequency is more than 1
-#         # print the element
-#         if(dict[item] > 1):
-#             print(item, end=" ")
-#
-#     print("\n")
-#
-# # Driver Code
-# if __name__ == "__main__":
-#     list = [12, 11, 40, 12,
-#             5, 6, 5, 12, 11]
-#     printDuplicates(list)
+def printDuplicates(arr):
+    dict = {}
+
+    for ele in arr:
+        try:
+            dict[ele] += 1
+        except:
+            dict[ele] = 1
+
+    for item in dict:
+
+        # if frequency is more than 1
+        # print the element
+        if(dict[item] > 1):
+            print(item, end=" ")
+
+    print("\n")
+
+# Driver Code
+if __name__ == "__main__":
+    list = [12, 11, 40, 12,
+            5, 6, 5, 12, 11]
+    printDuplicates(list)
--- remove_all_duplicates.py
+++ remove_all_duplicates.py
@@ -1,82 +1,77 @@
 # Python 3 code to demonstrate
 # removing duplicated from list
-# using naive methods
+# using naive methods initializing list
+test_list = [1, 3, 5, 6, 3, 5, 6, 1]
+print ("The original list is : " +  str(test_list))
 
-# initializing list
-# test_list = [1, 3, 5, 6, 3, 5, 6, 1]
-# print ("The original list is : " +  str(test_list))
-#
-# # using naive method
-# # to remove duplicated
-# # from list
-# res = []
-# for i in test_list:
-#     if i not in res:
-#         res.append(i)
-#
-# # printing list after removal
-# print ("The list after removing duplicates : " + str(res))
+# using naive method
+# to remove duplicated
+# from list
+res = []
+for i in test_list:
+    if i not in res:
+        res.append(i)
+
+# printing list after removal
+print ("The list after removing duplicates : " + str(res))
 
 
 
 # List comprehension
-# # Python 3 code to demonstrate
-# # removing duplicated from list
-# # using list comprehension
-#
-# # initializing list
-# test_list = [1, 3, 5, 6, 3, 5, 6, 1]
-# print ("The original list is : " +  str(test_list))
-#
-# # using list comprehension
-# # to remove duplicated
-# # from list
-# res = []
-# [res.append(x) for x in test_list if x not in res]
-#
-# # printing list after removal
-# print ("The list after removing duplicates : " + str(res))
+# Python 3 code to demonstrate
+# removing duplicated from list
+# using list comprehension
 
+# initializing list
+test_list = [1, 3, 5, 6, 3, 5, 6, 1]
+print ("The original list is : " +  str(test_list))
 
+# using list comprehension
+# to remove duplicated
+# from list
+res = []
+[res.append(x) for x in test_list if x not in res]
+# printing list after removal
+print ("The list after removing duplicates : " + str(res))
 
 
 # Set
 # Python 3 code to demonstrate
 # removing duplicated from list
 # using list comprehension
+
+# initializing list
+test_list = [1, 3, 5, 6, 3, 5, 6, 1]
+print ("The original list is : " +  str(test_list))
+
+# using list comprehension
+# to remove duplicated
+# from list
+res = []
+[res.append(x) for x in test_list if x not in res]
+
+# printing list after removal
+print ("The list after removing duplicates : " + str(res))
+
+
+
 #
-# # initializing list
-# test_list = [1, 3, 5, 6, 3, 5, 6, 1]
-# print ("The original list is : " +  str(test_list))
-#
-# # using list comprehension
-# # to remove duplicated
-# # from list
-# res = []
-# [res.append(x) for x in test_list if x not in res]
-#
-# # printing list after removal
-# print ("The list after removing duplicates : " + str(res))
-
-
-
-
 # List comp and enumerate
 # Python 3 code to demonstrate
 # removing duplicated from list
 # using list comprehension + enumerate()
-
+#
 # initializing list
-# test_list = [1, 5, 3, 6, 3, 5, 6, 1]
-# print ("The original list is : " +  str(test_list))
-#
-# # using list comprehension + enumerate()
-# # to remove duplicated
-# # from list
-# res = [i for n, i in enumerate(test_list) if i not in test_list[:n]]
-#
-# # printing list after removal
-# print ("The list after removing duplicates : " + str(res))
+test_list = [1, 5, 3, 6, 3, 5, 6, 1]
+print ("The original list is : " +  str(test_list))
+
+# using list comprehension + enumerate()
+# to remove duplicated
+# from list
+res = [i for n, i in enumerate(test_list) if i not in test_list[:n]]
+
+# printing list after removal
+print ("The list after removing duplicates : " + str(res))
 
 
 
@@ -85,19 +80,19 @@
 
 
 # collectios.orderedDict.fromkeys()
-# Python 3 code to demonstrate 
-# removing duplicated from list 
-# # using collections.OrderedDict.fromkeys()
-# from collections import OrderedDict
-# 
-# # initializing list
-# test_list = [1, 5, 3, 6, 3, 5, 6, 1]
-# print ("The original list is : " +  str(test_list))
-# 
-# # using collections.OrderedDict.fromkeys()
-# # to remove duplicated 
-# # from list 
-# res = list(OrderedDict.fromkeys(test_list))
-# 
-# # printing list after removal 
-# print ("The list after removing duplicates : " + str(res))
+# Python 3 code to demonstrate
+# removing duplicated from list
+# using collections.OrderedDict.fromkeys()
+from collections import OrderedDict
+
+# initializing list
+test_list = [1, 5, 3, 6, 3, 5, 6, 1]
+print ("The original list is : " +  str(test_list))
+
+# using collections.OrderedDict.fromkeys()
+# to remove duplicated
+# from list
+res = list(OrderedDict.fromkeys(test_list))
+
+# printing list after removal
+print ("The list after removing duplicates : " + str(res))
